14e90fc01a3b89c1bfb98962a570daa9c27f52b1
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-11-20 19:11:48 +0800

    feat: components

--- core/component/component.go
+++ core/component/component.go
@@ -1,6 +1,31 @@
 package component
 
-type Component interface {
-	Inputs() []Pin
-	Outputs() []Pin
+import (
+	"fmt"
+
+	"github.com/marcbentoy/verifast/core/gate"
+)
+
+type Component struct {
+	Ins   []*gate.Pin
+	Outs  []*gate.Pin
+	Gates []*gate.Gate
+}
+
+func NewComponent(ins, outs []*gate.Pin, gates []*gate.Gate) *Component {
+	return &Component{
+		Ins:   ins,
+		Outs:  outs,
+		Gates: gates,
+	}
+}
+
+func (c *Component) Evaluate() {
+	for _, g := range c.Gates {
+		g.Evaluate()
+	}
+}
+
+func (c *Component) String() string {
+	return fmt.Sprintf("in: %+v | out: %+v", c.Ins, c.Outs)
 }
