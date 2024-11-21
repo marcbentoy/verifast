d1b246b608c50948a56402b8c38c463190d2f71a
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-11-21 08:52:42 +0800

    up

--- cmd/main.go
+++ cmd/main.go
@@ -4,28 +4,61 @@ import (
 	"fmt"
 
 	"github.com/marcbentoy/verifast/core/component"
+	"github.com/marcbentoy/verifast/core/gate"
 )
 
 func main() {
 	fmt.Println("verifast by marcbentoy")
+	fmt.Println()
+	fmt.Println()
 
-	nandIns := []component.Pin{
-		*component.NewPin(0),
-		*component.NewPin(1),
-	}
-	nandOuts := []component.Pin{
-		*component.NewPin(0),
-		*component.NewPin(0),
-	}
-	nand := component.NewGate(nandIns, nandOuts, nandFunc)
-	nand.Evaluate()
+	// pins
+	// nand pins
+	pAi := gate.NewPin(0)
+	pBi := gate.NewPin(0)
+	pCi := gate.NewPin(1)
+	// and pins
+	pAo := gate.NewPin(0)
+	pBo := gate.NewPin(0)
+	// or pins
+	pCo := gate.NewPin(0)
+
+	nandG := gate.NewGate([]*gate.Pin{pAi, pBi}, []*gate.Pin{pAo}, nandFunc)
+	andG := gate.NewGate([]*gate.Pin{pAo, pCi}, []*gate.Pin{pBo}, andFunc)
+	orG := gate.NewGate([]*gate.Pin{pBo, pCi}, []*gate.Pin{pCo}, orFunc)
+
+	testComponent := component.NewComponent([]*gate.Pin{pAi, pBi, pCi}, []*gate.Pin{pCo}, []*gate.Gate{nandG, andG, orG})
+
+	fmt.Println("component before evaluation: ", *testComponent.Outs[0])
+	testComponent.Evaluate()
+	fmt.Println("component after evaluation: ", *testComponent.Outs[0])
+
+	fmt.Println("nandG: ", *nandG.Outs[0])
+	fmt.Println("andG: ", *andG.Outs[0])
+	fmt.Println("orG: ", *orG.Outs[0])
 }
 
-func nandFunc(g *component.Gate) {
+func nandFunc(g *gate.Gate) {
 	if (g.Ins[0].Value == 0) && (g.Ins[1].Value == 0) {
 		g.Outs[0].Value = 1
 	} else {
-		g.Outs[0].Value = 1
+		g.Outs[0].Value = 0
 	}
-	fmt.Println("this is a function inside nand with ins: ", g.Ins)
+}
+
+func andFunc(g *gate.Gate) {
+	if (g.Ins[0].Value == 1) && (g.Ins[1].Value == 1) {
+		g.Outs[0].Value = 1
+	} else {
+		g.Outs[0].Value = 0
+	}
+}
+
+func orFunc(g *gate.Gate) {
+	if (g.Ins[0].Value == 1) || (g.Ins[1].Value == 1) {
+		g.Outs[0].Value = 1
+	} else {
+		fmt.Println("false bai")
+		g.Outs[0].Value = 0
+	}
 }
